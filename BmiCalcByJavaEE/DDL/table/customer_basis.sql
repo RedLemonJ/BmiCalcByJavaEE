ea735ad97d40f3677ef314edd6ec87283e7683dc
K.yamamoto <31173803+RedLemonJ@users.noreply.github.com>
2020-04-02 11:14:12 +0900

    DDL修正 テーブルは一つにする

--- BmiCalcByJavaEE/DDL/table/customer_basis.sql
+++ BmiCalcByJavaEE/DDL/table/customer_basis.sql
@@ -3,5 +3,8 @@ create table customer_basis
 	customer_id varchar(10),
 	name varchar(30),
 	birth_day varchar(8),
+	height numeric(3,2),
+	weight numeric(5,2),
+	bmi    numeric(4,2),
 	primary key (customer_id)
 );
--- BmiCalcByJavaEE/DDL/table/customer_physical.sql
+++
@@ -1,8 +0,0 @@
-create table customer_physical
-(
-	customer_id varchar(10),
-	height numeric(3,2),
-	weight numeric(5,2),
-	bmi    numeric(4,2),
-	primary key (customer_id)
-);
